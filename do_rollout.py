4ca05f02f54a44b8a77083b5e7303c68733be539
Traiano Welcome <archbungle@users.noreply.github.com>
2016-09-01 05:55:44 +0400

    Some minor additions: Get network details of droplet. List ssh keys available in account

--- do_rollout.py
+++ do_rollout.py
@@ -1,24 +1,21 @@
 #Python code to build a basic droplet using D.O API
 #And do basic lifecycle management of the droplet
 #traiano, 31 August 2016
-#Install python digital ocean module as follows:
-#You can install python-digitalocean using **pip**
-#pip install -U python-digitalocean
-#Replace "your_personal_do_token" with your personal
-#digital ocean token (first get an account)
-
 import digitalocean
-my_token="your_personal_do_token"
+my_token="0073ae2d8412fda6232931dfc6e37dcbd3854d632fa8b925fcb93a1a8657e458"
+my_sshkey_id="do-primary-key"
 
 def operations_menu():
  status=0
- while(status != 6):
+ while(status != 7):
   print "1] Enter 1 for a list of droplets."
   print "2] Enter 2 to poweroff all nodes."
   print "3] Enter 3 startup all nodes."
   print "4] Enter 4 delete all droplets."
   print "5] Enter 5 to create a droplet"
-  print "6] Enter 6 to quit."
+  print "6] Enter 6 to get droplet details"
+  print "7] Enter 7 to quit."
+  print "8] Enter 8 to list all ssh keys."
   option=int(raw_input("option> "))
   if(option==1):
    list_all_droplets()
@@ -33,7 +30,13 @@ def operations_menu():
    d_name.strip("_")				# for anyone tempted to use underscores
    build_new_droplet(d_name) 
   elif(option==6):
-   status=6
+   d_name=str(raw_input("Enter droplet name: "))
+   d_name.strip("_")				# for anyone tempted to use underscores
+   get_all_details(d_name)
+  elif(option==8):
+   get_all_keys()
+  elif(option==7):
+   status=7
    continue
   else:
    print "invalid option"
@@ -41,11 +44,13 @@ def operations_menu():
 def build_new_droplet(d_name):
  droplet = digitalocean.Droplet(token=my_token,
  name=d_name,
- region='nyc2', 
- image='ubuntu-14-04-x64', 
- size_slug='512mb', 
+ region='nyc2',
+ image='ubuntu-14-04-x64',
+ size_slug='512mb',
+ ssh_key_ids=[my_sshkey_id],
  backups=True)
- droplet.create()
+ results=droplet.create()
+ print "droplet data> ",results
 
 def check_droplet_status(droplet):
  actions = droplet.get_actions()
@@ -53,6 +58,20 @@ def check_droplet_status(droplet):
   action.load()
   print "droplet: ",droplet,"action: ",action," Action Status: ",action.status
 
+def get_all_details(d_name):
+ manager = digitalocean.Manager(token=my_token)
+ droplets = manager.get_all_droplets()
+ for droplet in droplets:
+  if(droplet.name == d_name):
+   droplet_data=droplet.load()
+   print "droplet: ",d_name," network data: ",droplet_data.ip_address
+
+def get_all_keys():
+ manager = digitalocean.Manager(token=my_token)
+ ssh_keys=manager.get_all_sshkeys()
+ for key in ssh_keys:
+  print "ssh key: ",key
+ 
 def shutdown_droplets():
  manager = digitalocean.Manager(token=my_token)
  droplets = manager.get_all_droplets()
